f7f7e23697cc5e891631abffceb8932a884a006e
zhuang002 <zhuang001@gmail.com>
2021-02-06 18:09:14 -0500

    Draft

---
+++ PrintfExample/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=2,b=3;
+		double d1=2.3, d2=4.455; 
+		double d3=d2/d1;
+		String s1="abcd safa";
+		String s2="ABFD SDFE";
+		
+		System.out.printf("%-10s%10s%-10s%10s%10s\n%10d%10d%-10.2f%10.2f%10.2f\ns1=\"%s\"\ns2=\"%s\"","a","b","d1","d2","d3",a,b,d1,d2,d3,s1,s2);
+
+	}
+
+}
